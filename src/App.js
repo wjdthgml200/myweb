b4254eeffb59e4e4a041e0e87b22ce6f8cb2867f
wjdthgml200 <61775446+wjdthgml200@users.noreply.github.com>
2020-08-13 19:38:03 +0900

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import {Button, Progress} from 'semantic-ui-react'
+
+function App() {
+  return (
+    <div>
+<p> 안녕하세요</p>
+<Button primary> 하이하이 </Button>
+<Progress percent = {66} />
+</div>
+  );
+}
+
+export default App;
